1e841f696bb671be47b21763fdaccf49eb95eb80
wml <wmlbuaa@163.com>
2018-11-07 17:29:21 +0800

    ubuntu

--- base_newFscore.py
+++ base_newFscore.py
@@ -8,7 +8,7 @@ import math
 warnings.filterwarnings('ignore')
 n_splits = 10
 seed = 1030
-train = pd.read_csv('train_2_dup_del.csv')
+train = pd.read_csv('train_2_fresh.csv')
 test = pd.read_csv('test_2.csv')
 
 
@@ -17,7 +17,6 @@ def f1_score(pred, data):
     pred = np.argmax(pred.reshape(11, -1), axis=0)
     k, f1 = 0, 0
     precision, recall, TP, FP, FN = [0] * 11, [0] * 11, [0] * 11, [0] * 11, [0] * 11
-
     for kl in label:
         kp = pred[k]
         if kp == kl:
@@ -28,61 +27,46 @@ def f1_score(pred, data):
         k += 1
 
     for j in range(0, 11):
-        precision[j] = TP[j] / (TP[j] + FP[j])
-        recall[j] = TP[j] / (TP[j] + FN[j])
-        f1 += (2 * precision[j] * recall[j]) / (precision[j] + recall[j])
-
-    score = math.pow((1.00 / 11.00) * f1, 2)
-
-    return 'f1_score', score
-
-
-params = {
-    "learning_rate": 0.1,
-    "lambda_l1": 0.1,
-    "lambda_l2": 0.2,
-    "max_depth": 10,
-    "num_class": 11,
-    "objective": "multiclass",
-    "num_leaves": 63,
-    'num_threads': 4,
-    'max_bin': 511,
-    'metric': None
-}
-
-
-def f1_score(pred, data):
-    label = data.get_label()
-    pred = np.argmax(pred.reshape(11, -1), axis=0)
-    k, f1 = 0, 0
-    precision, recall, TP, FP, FN = [0] * 11, [0] * 11, [0] * 11, [0] * 11, [0] * 11
-
-    for kl in label:
-        kp = pred[k]
-        if kp == kl:
-            TP[kp] += 1
+        if (TP[j] + FP[j] != 0):
+            precision[j] = TP[j] / (TP[j] + FP[j])
         else:
-            FP[kp] += 1
-            FN[kl] += 1
-        k += 1
+            precision[j] = 0
+        if (TP[j] + FN[j] != 0):
+            recall[j] = TP[j] / (TP[j] + FN[j])
+        else:
+            recall[j] = 0;
 
-    for j in range(0, 11):
-        precision[j] = TP[j] / (TP[j] + FP[j])
-        recall[j] = TP[j] / (TP[j] + FN[j])
-        f1 += (2 * precision[j] * recall[j]) / (precision[j] + recall[j])
+        if (precision[j] + recall[j] != 0):
+            f1 += (2 * precision[j] * recall[j]) / (precision[j] + recall[j])
+        else:
+            f1 += 0;
 
     score = math.pow((1.00 / 11.00) * f1, 2)
 
     return 'f1_score', score, True
 
 
+params = {
+    "learning_rate": 0.05,
+    "lambda_l1": 0.1,
+    "lambda_l2": 0.2,
+    "max_depth": 10,
+    "num_class": 11,
+    'application': "multiclass",
+    'boosting': 'gbdt',
+    "num_leaves": 63,
+    'num_threads': 8,
+    'max_bin': 511
+}
+
+'''
 def logregobj(preds, dtrain):
     labels = dtrain.get_label()
     preds = 1.0 / (1.0 + np.exp(-preds))
     grad = preds - labels
     hess = preds * (1.0 - preds)
     return grad, hess
-
+'''
 
 # 对标签编码 映射关系
 label2current_service = dict(
@@ -110,15 +94,15 @@ cv_pred = []
 
 skf = StratifiedKFold(n_splits=n_splits, random_state=seed, shuffle=True)  # sklearn divide 10
 for index, (train_index, test_index) in enumerate(skf.split(X, y)):  # 后一项为元组
-
+    print(index)
     X_train, X_valid, y_train, y_valid = X[train_index], X[test_index], y[train_index], y[test_index]
     train_data = lgb.Dataset(X_train, label=y_train)
     validation_data = lgb.Dataset(X_valid, label=y_valid)
 
-    clf = lgb.train(params, train_data, num_boost_round=100000, valid_sets=[validation_data], early_stopping_rounds=200,
+    clf = lgb.train(params, train_data, num_boost_round=100000, valid_sets=[validation_data], early_stopping_rounds=500,
                     feval=f1_score, verbose_eval=10)
 
-    y_test = clf.predict(X_test, num_iteration=clf.best_iteration+200)
+    y_test = clf.predict(X_test, num_iteration=clf.best_iteration + 400)
     y_test = [np.argmax(x) for x in y_test]
 
     if index == 0:
